206fa1fcf86796728478cfd2a9aef4ca7d8941d0
wpslxm20 <gosumin0710@naver.com>
2025-09-03 20:31:13 +0900

    [FEAT] AppHeader 구현 #2

--- presentation/src/main/java/com/arambyeol/presentation/MainActivity.kt
+++ presentation/src/main/java/com/arambyeol/presentation/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.arambyeol.presentation.ui.theme.Arambyeol_ver3Theme
+import com.arambyeol.presentation.ui.theme.TransparentYellow
 import com.arambyeol.todaymeal.TodayMealScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +28,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Arambyeol_ver3Theme {
-                TodayMealScreen()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    contentWindowInsets = WindowInsets.systemBars, // system bar inset 적용
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(color = TransparentYellow),
+                        ) {
+                            AppHeader()
+                        }
+
+//                      TodayMealScreen()
+                    }
+                }
             }
         }
     }

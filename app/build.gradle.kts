5a9d23c4e0847b6fccdedf69da559c6914897e55
wpslxm20 <gosumin0710@naver.com>
2025-10-16 22:28:22 +0900

    [CHORE] 빌드 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.arambyeol.arambyeol_ver3"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
